23eb2ad6c4d67a1f9470ff6ed1bc7138b43e3c7f
gerelef <gerelef@gmail.com>
2022-08-31 13:40:26 +0300

    somewhat finished top box skeleton

--- ui.py
+++ ui.py
@@ -1,12 +1,10 @@
 import gi
 
 gi.require_version("Gtk", "4.0")
-from gi.repository import Gtk, Gio
+from gi.repository import Gtk, Gdk
 
 
 # TODO make arrow buttons ignore GNOME app row
-# TODO on lose focus, instantly combust (and die)
-# TODO on minimize, instantly combust (and die)
 # TODO when the input text is overfilled, expand the main window until a maximum of 1280 pixels...
 #  otherwise, instantly combust and die (no, don't, this is a joke, just let it scroll)
 # TODO only handle TEXT mime on drag & drop
@@ -15,6 +13,7 @@ from gi.repository import Gtk, Gio
 # TODO when it's a program on $PATH, and going up & down with the arrows, store the current input text at the "top row"
 #  like a reverse bash termux; if we're going to execute a specific thing, show that on the input text, and make it
 #  editable (so it can be executed with more parameters)...
+# TODO add full accessibility support
 
 # TODO create singleton metaclass as model
 # TODO create controller to simplify interactions for MVC w/ dependency injection (Model(Controller(View)))
@@ -23,6 +22,7 @@ from gi.repository import Gtk, Gio
 # TODO add flag to launch UI on initial application launch; if it's not set, we're running as a session service...
 #  otherwise, we make all initializations on first application launch, & we output errors to a dumpfile on path...
 #  idk where though right now but ok
+# TODO when running as a service, make sure to demote starting processes to userspace, and not root...
 
 # https://zetcode.com/python/gtk/
 class GRunner(Gtk.ApplicationWindow):
@@ -32,10 +32,15 @@ class GRunner(Gtk.ApplicationWindow):
 
         self.entry = None
         self.action_label = None
-        self.settings_button = None
+
+        self.default_width = 500
+        self.default_height = 200
+        self.current_width = 500
+        self.current_height = 200
 
         self.gnome_box = None
         self.top_box = None
+        self.content_box = None
         self.wrapper_box = None
 
         self.init_ui(app)
@@ -47,12 +52,17 @@ class GRunner(Gtk.ApplicationWindow):
         self.set_default_size(500, 200)
 
         self.action_label = Gtk.Label()
-        self.action_label.set_markup(f"<i>Switching</i> to <b>None</b>")
+        self.action_label.set_markup(f"Switching to <b>None</b>")
         self.action_label.set_halign(Gtk.Align.CENTER)
 
+        # https://specifications.freedesktop.org/icon-naming-spec/icon-naming-spec-latest.html
         self.entry = Gtk.Entry()
+        self.entry.set_icon_from_icon_name(Gtk.EntryIconPosition.PRIMARY, "edit-delete")
+        self.entry.set_icon_from_icon_name(Gtk.EntryIconPosition.SECONDARY, "emblem-system")
+        self.entry.set_placeholder_text("...")
         self.entry.set_margin_start(0)
         self.entry.set_margin_end(0)
+        self.entry.connect('icon-press', self.handle_entry_icon_buttons)
 
         # FIXME add 5 buttons with the top 5 most used/called gnome apps from this app (and key them to /1/2/3/4/5)
         btn1 = Gtk.Button(label="")  # FIXME THIS SHOULD BE A GNOME APP
@@ -60,8 +70,11 @@ class GRunner(Gtk.ApplicationWindow):
         btn3 = Gtk.Button(label="")  # FIXME THIS SHOULD BE A GNOME APP
         btn4 = Gtk.Button(label="")  # FIXME THIS SHOULD BE A GNOME APP
         btn5 = Gtk.Button(label="")  # FIXME THIS SHOULD BE A GNOME APP
-        # https://specifications.freedesktop.org/icon-naming-spec/icon-naming-spec-latest.html
-        self.settings_button = Gtk.Button.new_from_icon_name("emblem-system")
+        btn1.connect('clicked', lambda _: self.set_default_size(500, 200))  # TODO CONVERT TO CONTROLLER
+        btn2.connect('clicked', lambda _: self.set_default_size(500, 400))  # TODO CONVERT TO CONTROLLER
+        btn3.connect('clicked', lambda _: self.set_default_size(500, 600))  # TODO CONVERT TO CONTROLLER
+        btn4.connect('clicked', lambda _: self.set_default_size(500, 800))  # TODO CONVERT TO CONTROLLER
+        btn5.connect('clicked', lambda _: self.entry.set_text(str(self.get_default_size())))
 
         self.gnome_box = Gtk.Box.new(Gtk.Orientation.HORIZONTAL, 25)  # 25*4 100px gap
         self.gnome_box.set_margin_start(20)
@@ -72,7 +85,6 @@ class GRunner(Gtk.ApplicationWindow):
         self.gnome_box.append(btn3)
         self.gnome_box.append(btn4)
         self.gnome_box.append(btn5)
-        self.gnome_box.append(self.settings_button)
 
         self.top_box = Gtk.Box.new(Gtk.Orientation.VERTICAL, 20)
         self.top_box.set_margin_start(20)
@@ -87,12 +99,35 @@ class GRunner(Gtk.ApplicationWindow):
 
         splitter = Gtk.Separator()
 
+        self.content_box = Gtk.Box.new(Gtk.Orientation.VERTICAL, 5)
+
         self.wrapper_box = Gtk.Box.new(Gtk.Orientation.VERTICAL, 0)
         self.wrapper_box.append(self.top_box)
         self.wrapper_box.append(splitter)
+        self.wrapper_box.append(self.content_box)
 
         # noinspection PyUnresolvedReferences
         self.set_child(self.wrapper_box)
+
+        # https://docs.gtk.org/gtk4/class.EventControllerFocus.html
+        win_event_focus_controller = Gtk.EventControllerFocus()
+        win_event_focus_controller.connect("enter", lambda x: print(x))
+        win_event_focus_controller.connect("leave", lambda x: self.close())
+        self.add_controller(win_event_focus_controller)
+
+    def handle_entry_icon_buttons(self, x, y):
+        print(x, y)
+        if y == Gtk.EntryIconPosition.PRIMARY:
+            self.empty_entry()
+        if y == Gtk.EntryIconPosition.SECONDARY:
+            self.show_settings()
+
+    def show_settings(self):
+        print("Showing settings modal")
+
+    def empty_entry(self):
+        print("Deleting text")
+        self.entry.set_text("")
 
 
 def on_activate(app):
@@ -100,7 +135,11 @@ def on_activate(app):
     win.present()
 
 
-if __name__ == "__main__":
+def start_ui():
     app = Gtk.Application(application_id='foss.gRunner')
     app.connect('activate', on_activate)
     app.run(None)
+
+
+if __name__ == "__main__":
+    start_ui()
